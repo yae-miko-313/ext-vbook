65732295289ae9186a8d7c4bf5c796fc7c529d89
LÊ HỒNG KỲ <lehongky.it@gmail.com>
2026-03-30 20:58:17 +0700

    chore(security): remove WebNovel.vn extension and rebuild catalogs

--- extensions/novel/webnovel-vn/src/home.js
+++
@@ -1,9 +0,0 @@
-function execute() {
-    return Response.success([
-        { title: "Mới Cập Nhật", input: "https://webnovel.vn/all/", script: "gen.js" },
-        { title: "Tiên Hiệp", input: "https://webnovel.vn/tien-hiep/", script: "gen.js" },
-        { title: "Ngôn Tình", input: "https://webnovel.vn/ngon-tinh/", script: "gen.js" },
-        { title: "Truyện Full", input: "https://webnovel.vn/truyen-full/", script: "gen.js" },
-        { title: "Sách Hay", input: "https://webnovel.vn/sach-hay/", script: "gen.js" }
-    ]);
-}
