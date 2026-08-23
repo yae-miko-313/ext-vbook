a8ab3b4c3bfd53efbccbccecb134254000ca16f1
hieunm3103 <hieunm3103@gmail.com>
2026-08-23 23:16:24 +0700

    feat: update Viet69 plugin version and refine home.js menu options

--- extensions/video/viet69/src/home.js
+++ extensions/video/viet69/src/home.js
@@ -1,14 +1,8 @@
 function execute() {
     return Response.success([
         { title: "Mới nhất", input: "/", script: "search.js" },
-        { title: "Video thành viên", input: "/bai-viet/", script: "search.js" },
-        { title: "Phim sex Việt", input: "/sex-viet/", script: "search.js" },
-        { title: "Sinh viên", input: "/sinh-vien/", script: "search.js" },
-        { title: "Teen", input: "/teen/", script: "search.js" },
-        { title: "Check hàng", input: "/check-hang/", script: "search.js" },
-        { title: "Camera", input: "/camera/", script: "search.js" },
-        { title: "BDSM", input: "/bdsm/", script: "search.js" },
-        { title: "Máy bay bà già", input: "/may-bay-ba-gia/", script: "search.js" },
-        { title: "Thủ dâm", input: "/thu-dam/", script: "search.js" }
+        { title: "Yêu thích nhất", input: "/yeu-thich/", script: "search.js" },
+        { title: "Xem nhiều nhất", input: "/xem-nhieu/", script: "search.js" },
+        { title: "Bình luận nhiều nhất", input: "/binh-luan/", script: "search.js" }
     ]);
 }
